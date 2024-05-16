7cf68361b57efc0e4f6e15631b3d30c388d75ddd
jmulq <james.mulqueeny@outlook.com>
2024-05-16 18:06:05 +0200

    feat: implement account entity

--- subgraph/src/mapping.ts
+++ subgraph/src/mapping.ts
@@ -6,7 +6,7 @@ import {
   log,
 } from "@graphprotocol/graph-ts";
 import * as assembly from "./assembly";
-import { Transfer } from "../generated/schema";
+import { Account, Transfer } from "../generated/schema";
 
 export function handleTransfers(bytes: Uint8Array): void {
   let transfers = assembly.contract.v1.Transfers.decode(bytes.buffer);
@@ -21,15 +21,29 @@ export function handleTransfers(bytes: Uint8Array): void {
         "-" +
         transferData.evt_index.toString();
 
-      let entity = new Transfer(transferId);
-      entity.evt_tx_hash = transferData.evt_tx_hash.toString();
-      entity.evt_index = BigInt.fromU32(transferData.evt_index);
-      entity.evt_block_time = transferData.evt_block_time.seconds.toString();
-      entity.evt_block_number = BigInt.fromU64(transferData.evt_block_number);
-      entity.from = transferData.from;
-      entity.to = transferData.to;
-      entity.value = BigDecimal.fromString(transferData.value);
-      entity.save();
+      let transfer = new Transfer(transferId);
+      transfer.evt_tx_hash = transferData.evt_tx_hash.toString();
+      transfer.evt_index = BigInt.fromU32(transferData.evt_index);
+      transfer.evt_block_time = transferData.evt_block_time.seconds.toString();
+      transfer.evt_block_number = BigInt.fromU64(transferData.evt_block_number);
+      transfer.from = transferData.from;
+      transfer.to = transferData.to;
+      transfer.value = BigDecimal.fromString(transferData.value);
+      transfer.save();
+
+      let fromAccount = Account.load(transferData.from);
+      if (fromAccount == null) {
+        fromAccount = new Account(transferData.from);
+      }
+      fromAccount.grt_balance = BigInt.fromString(transferData.from_balance);
+      fromAccount.save();
+
+      let toAccount = Account.load(transferData.to);
+      if (toAccount == null) {
+        toAccount = new Account(transferData.to);
+      }
+      toAccount.grt_balance = BigInt.fromString(transferData.to_balance);
+      toAccount.save();
     }
   }
 }
