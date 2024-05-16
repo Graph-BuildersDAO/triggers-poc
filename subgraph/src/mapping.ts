cddf01090e23473ea7a92c44a59606cb2dd70cd7
jmulq <james.mulqueeny@outlook.com>
2024-05-16 18:31:03 +0200

    feat: add normal subgraph

--- subgraph/src/mapping.ts
+++ subgraph/src/mapping.ts
@@ -7,6 +7,7 @@ import {
 } from "@graphprotocol/graph-ts";
 import * as assembly from "./assembly";
 import { Account, Transfer } from "../generated/schema";
+import { Transfer as TransferEvent } from "../generated/grt/GRT";
 
 export function handleTransfers(bytes: Uint8Array): void {
   let transfers = assembly.contract.v1.Transfers.decode(bytes.buffer);
@@ -45,5 +46,50 @@ export function handleTransfers(bytes: Uint8Array): void {
       toAccount.grt_balance = BigInt.fromString(transferData.to_balance);
       toAccount.save();
     }
+  }
+}
+
+export function handleTransfer(event: TransferEvent): void {
+  const to = event.params.to;
+  const from = event.params.from;
+  const value = event.params.value;
+
+  let receipt = event.receipt;
+  let txHash = "";
+  if (receipt != null) {
+    txHash = receipt.transactionHash.toHexString();
+  }
+  let transferId = txHash + "-" + event.logIndex.toString();
+  let transfer = new Transfer(transferId);
+
+  transfer.evt_tx_hash = txHash;
+  transfer.evt_index = event.logIndex;
+  transfer.evt_block_time = event.block.timestamp.toString();
+  transfer.evt_block_number = event.block.number;
+  transfer.from = from.toHexString();
+  transfer.to = to.toHexString();
+  transfer.value = value.toBigDecimal();
+  transfer.save();
+
+  let fromAccount = Account.load(from.toHexString());
+  if (fromAccount == null) {
+    fromAccount = new Account(from.toHexString());
+    fromAccount.grt_balance = BigInt.zero();
+    fromAccount.save();
+  } else {
+    let newBalance = fromAccount.grt_balance.minus(value);
+    fromAccount.grt_balance = newBalance;
+    fromAccount.save();
+  }
+
+  let toAccount = Account.load(to.toHexString());
+  if (toAccount == null) {
+    toAccount = new Account(to.toHexString());
+    toAccount.grt_balance = BigInt.zero();
+    toAccount.save();
+  } else {
+    let newBalance = toAccount.grt_balance.plus(value);
+    toAccount.grt_balance = newBalance;
+    toAccount.save();
   }
 }
